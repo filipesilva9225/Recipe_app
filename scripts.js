711fb559bbe05f921171a7c369383b97aecf5a11
Filipe Silva <140545021+filipesilva9225@users.noreply.github.com>
2025-08-22 20:54:13 +0000

    final project

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-const input = document.querySelector(".search-input");
 const form = document.querySelector(".search-form");
 const recipeList = document.querySelector(".recipe-list");
+const recipeDetails = document.querySelector(".recipe-details");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -35,4 +35,29 @@ async function getRecipesDetails(id) {
     `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
   );
   const data = await response.json();
+  const recipe = data.meals[0];
+
+  let ingredients = "";
+
+  for (let i = 1; i <= 20; i++) {
+    if (recipe[`strIngredient${i}`]) {
+      ingredients += `<li>${recipe[`strIngredient${i}`]} - ${
+        recipe[`strMeasure${i}`]
+      }</li>`;
+    } else {
+      break;
+    }
+  }
+  recipeDetails.innerHTML = `
+  <h2>${recipe.strMeal}</h2>
+  <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}"></img>
+  <h3>Categoria: ${recipe.strCategory}</h3>
+  <h3>Origem: ${recipe.strArea}</h3>
+  <h3>Ingredientes:</h3>
+  <ul>${ingredients}</ul>
+  <h3>Instruções:</h3>
+  <p>${recipe.strInstructions}</p>
+  <p>${recipe.strTags}</p>
+  <p>Vídeo: <a href="${recipe.strYoutube}" target="_blank"> Assista no Youtube</a></p>
+  `;
 }
